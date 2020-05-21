4ed159f404b60c8ca66ffdc95e89559f20750855
kian79 <kiankr79@gmail.com>
2020-05-21 19:23:22 +0430

    Finished a_star

--- q2.py
+++ q2.py
@@ -1,6 +1,8 @@
 from typing import List, Tuple
 from copy import deepcopy
 
+created = 0
+developed = 0
 
 class Board:
     def __init__(self, inputs: list, square_pos):
@@ -28,9 +30,6 @@ class Board:
                     max_v = v
             result += sum_v - max_v
         return result
-
-
-
 
 
 class Node:
@@ -74,32 +73,30 @@ class Node:
                 # print_list("new input : ",new_input)
                 new_input[temp_sq[0]][temp_sq[1]] = new_input[next_node_sq[0]][next_node_sq[1]]
                 new_input[next_node_sq[0]][next_node_sq[1]] = '#'
-                next_node = Node(Board(new_input, next_node_sq),self.G+1)
+                next_node = Node(Board(new_input, next_node_sq), self.G + 1)
                 next_node.parent = self
                 self.nexts.append(next_node)
 
-def is_in_explored(a_node:Node,explored:List[Node]):
+
+def is_in_explored(a_node: Node, explored: List[Node]):
     for exp in explored:
-        if a_node.board.inputs==exp.board.inputs:
+        if a_node.board.inputs == exp.board.inputs:
             return True
         return False
 
 
-def is_in_frontier(a_node:Node, frontier:List[Node]):
-    for front in frontier:
-        if a_node.board.inputs==front.board.inputs and 
-
-
-def a_star(node : Node):
-    frontier : List[Node]=[]
-    explored : List[Node]=[]
+def a_star(node: Node):
+    frontier: List[Node] = []
+    explored: List[Node] = []
     frontier.append(node)
-    current_node : Node = min(frontier,key=lambda x:x.heuristic + x.G)
+    current_node: Node = min(frontier, key=lambda x: x.heuristic + x.G)
     while frontier:
+        global developed
+        developed+=1
         if current_node.is_goal():
-            path : List[Node] = [current_node]
+            path: List[Node] = [current_node]
             while current_node.parent:
-                path.insert(0,current_node.parent)
+                path.insert(0, current_node.parent)
                 current_node = current_node.parent
             return path
         else:
@@ -107,6 +104,50 @@ def a_star(node : Node):
             explored.append(current_node)
             current_node.produce_nexts()
             for child in current_node.nexts:
-                if not is_in_explored(a_node=child,explored=explored):
-                    if not is_in_frontier(a_node=child,frontier=frontier)
+                global created
+                created+=1
+                flag = False
+                if not is_in_explored(a_node=child, explored=explored):
+                    for front in frontier:
+                        if child.board.inputs != front.board.inputs or child.heuristic + child.G < front.heuristic + front.G:
+                            flag = True
+                            break
+                    if flag:
+                        frontier.append(child)
 
+
+if __name__ == '__main__':
+    row, col = map(int, input().split())
+    qu = []
+    sqr = ()
+    for i in range(row):
+        q1 = list(input().split())
+        nq1 = []
+        for j in range(col):
+            if q1[j] != '#':
+                nq1.append((int(q1[j][:3]), q1[j][3:]))
+            else:
+                sqr = (i, j)
+                nq1.append(q1[j])
+        qu.append(nq1)
+
+    my_node = Node(Board(qu, square_pos=sqr), 0)
+    frontier = a_star(my_node)
+    print(len(frontier) - 2)
+    last_sq = frontier[0].board.square_pos
+    dirs = []
+    for i in range(len(frontier)):
+        direction = (frontier[i].board.square_pos[0] - last_sq[0], frontier[i].board.square_pos[1] - last_sq[1])
+        if direction == (0, 1):
+            dirs.append('RIGHT')
+        elif direction == (0, -1):
+            dirs.append('LEFT')
+        elif direction == (1, 0):
+            dirs.append('DOWN')
+        elif direction == (-1, 0):
+            dirs.append("UP")
+        last_sq = (frontier[i].board.square_pos[0], frontier[i].board.square_pos[1])
+    for i in dirs:
+        print(i)
+    print("created nodes : ", created)
+    print("developed nodes : ", developed)
